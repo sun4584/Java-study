3de6e30fc499e8478586efd799a675047ae3abe9
sun4584 <sun4584@example.com>
2025-07-24 22:52:42 +0900

    JavaStart

---
+++ JavaStart/src/section02/printfSpace.java
@@ -0,0 +1,15 @@
+package section02;
+
+public class printfSpace {
+	public static void main(String[] args) {
+		System.out.printf("%5d", 1);
+		System.out.println();
+		System.out.printf("%5d", 12);
+		System.out.println();
+		System.out.printf("%5d", 123);
+		System.out.println();
+		System.out.printf("%5d", 1234);
+		System.out.println();
+		System.out.printf("%5d", 12345);
+	}
+}
