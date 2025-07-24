ff756c9b90a95be398bda02928c5c3d3115ed9af
sun4584 <sun4584@example.com>
2025-07-24 22:21:31 +0900

    JavaStart

---
+++ JavaStart/src/section02/ConsolePrint.java
@@ -0,0 +1,11 @@
+package section02;
+
+public class ConsolePrint {
+	public static void main(String[] args) {
+		// 괄호() 안의 데이터를 콘솔창에 입력
+		System.out.print("Welcome.");
+		// "Welcome." 문자열 옆에 "Java World" 문자열을 출력
+		System.out.print("Java World");
+	} 
+	
+}
---
+++ JavaStart/src/section02/ConsolePrint2.java
@@ -0,0 +1,10 @@
+package section02;
+
+public class ConsolePrint2 {
+	public static void main(String[] args) {
+		// 괄호() 안의 데이터를 콘솔창에 입력
+		System.out.println("Welcome.");
+		// "Welcome." 문자열 아래에 "Java World" 문자열을 출력
+		System.out.println("Java World");
+	} 
+}
